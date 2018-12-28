41a70d462a13e0844fdd81166b62571ccafe967b
Charlie Micou <charlesmicou@gmail.com>
2018-12-28 19:13:48 +0000

    Feature/snapshot from scene (#7)
    * Common static scenery script for auto instantiables
    
    * Implement snapshotting from a scene

--- modules/spatialos/snapshot_generator.cpp
+++ modules/spatialos/snapshot_generator.cpp
@@ -2,97 +2,69 @@
 #include <godotcore/godot_position2d.h>
 #include <improbable/standard_library.h>
 #include <godotcore/auto_instantiable.h>
-#include <spellcrest/moba_unit.h>
 #include "improbable/worker.h"
 #include "spatial_util.h"
 #include "component_registry.h"
-#include <iostream>
+#include "schema_parser.h"
 
-worker::Entity make_ball(float x, float y) {
-    worker::Entity builder;
+WorkerLogger SnapshotGenerator::logger = WorkerLogger("snapshot_generator");
 
-    godotcore::GodotCoordinates2D gpos({{0, 0}, {x, y}});
-    
-    builder.Add<improbable::Metadata>({"TestBall"});
-    builder.Add<improbable::Persistence>({});
-    builder.Add<godotcore::GodotPosition2D>({gpos, {}});
-    builder.Add<godotcore::AutoInstantiable>({"res://auto_scene/TestBall.tscn"});
-    builder.Add<improbable::Position>({fromGodotPosition(gpos)});
-    builder.Add<spellcrest::MobaUnit>({{50, 50}, "Test Ball"});
-
-    // ACL must be done last if we want it to pick up components automatically
-    builder.Add<improbable::EntityAcl>({clientAndServerReqSet, make_component_acl(builder, "")});
-    
-    return builder;
-}
-
-worker::Entity make_rock(float x, float y) {
-    worker::Entity builder;
-
-    godotcore::GodotCoordinates2D gpos({{0, 0}, {x, y}});
-    
-    builder.Add<improbable::Metadata>({"Rock"});
-    builder.Add<improbable::Persistence>({});
-    builder.Add<godotcore::GodotPosition2D>({gpos, {}});
-    builder.Add<godotcore::AutoInstantiable>({"res://auto_scene/rock1.tscn"});
-    builder.Add<improbable::Position>({fromGodotPosition(gpos)});
-
-    // ACL must be done last if we want it to pick up components automatically
-    builder.Add<improbable::EntityAcl>({clientAndServerReqSet, make_component_acl(builder, "")});
-    
-    return builder;
-}
-
-worker::Entity make_tree(float x, float y) {
-    worker::Entity builder;
-
-    godotcore::GodotCoordinates2D gpos({{0, 0}, {x, y}});
-    
-    builder.Add<improbable::Metadata>({"Tree"});
-    builder.Add<improbable::Persistence>({});
-    builder.Add<godotcore::GodotPosition2D>({gpos, {}});
-    builder.Add<godotcore::AutoInstantiable>({"res://auto_scene/tree1.tscn"});
-    builder.Add<improbable::Position>({fromGodotPosition(gpos)});
-
-    // ACL must be done last if we want it to pick up components automatically
-    builder.Add<improbable::EntityAcl>({clientAndServerReqSet, make_component_acl(builder, "")});
-    
-    return builder;
-}
-
-void SnapshotGenerator::takeSnapshot(String path) {
+void SnapshotGenerator::new_snapshot(String path) {
     std::string p = fromGodotString(path);
-    worker::SnapshotOutputStream ostream{MergedComponentRegistry(), p};
-    std::cout << "Writing snapshot to " << p << std::endl;
+    snapshot_ostream = new worker::SnapshotOutputStream{MergedComponentRegistry(), p};
+    logger.info("Writing snapshot to " + p);
+}
 
-    ostream.WriteEntity(1, make_ball(-100, -100));
-    ostream.WriteEntity(2, make_ball(100, 100));
-    ostream.WriteEntity(3, make_ball(-100, 100));
-    ostream.WriteEntity(4, make_ball(100, -100));
-    ostream.WriteEntity(5, make_ball(150, -155));
-    ostream.WriteEntity(6, make_rock(-200, -200));
-    ostream.WriteEntity(7, make_rock(200, 200));
-    ostream.WriteEntity(8, make_tree(300, 350));
-    ostream.WriteEntity(9, make_tree(-300, 0));
-    ostream.WriteEntity(10, make_tree(0, 500));
-    ostream.WriteEntity(11, make_tree(-355, 216));
-    ostream.WriteEntity(12, make_rock(-289, -465));
-    ostream.WriteEntity(13, make_rock(-351, -554));
-    ostream.WriteEntity(14, make_rock(-470, -586));
-    ostream.WriteEntity(15, make_ball(-863, -250));
-    ostream.WriteEntity(16, make_ball(-867, -164));
-    ostream.WriteEntity(17, make_ball(-800, -82));
-    ostream.WriteEntity(18, make_ball(-608, -224));
-    ostream.WriteEntity(19, make_ball(-583, -123));
-    ostream.WriteEntity(20, make_tree(-13, -361));
-    ostream.WriteEntity(21, make_tree(193, -261));
-    ostream.WriteEntity(22, make_tree(391, -451));
-    std::cout << "Finished writing snapshot" << std::endl;
+void SnapshotGenerator::add_auto_instantiable_entity(Node* node) {
+    std::string s = fromGodotString(node->get_name());
+    if (snapshot_ostream == nullptr) {
+        logger.error("Tried to add an entity to the snapshot without calling new_snapshot first");
+        return;
+    }
+    Node2D* n = dynamic_cast<Node2D*>(node);
+    if (n == nullptr) {
+        std::string offending_node = fromGodotString(node->get_name());
+        logger.warn("The node " + offending_node + " was not a Node2D, therefore has no position. Not adding it.");
+        return;
+    }
+    if (!n->has_method("_to_snapshot_entity")) {
+        std::string offending_node = fromGodotString(node->get_name());
+        logger.warn("The node " + offending_node + " has no method _to_snapshot_entity. Not adding it.");
+        return;
+    }
+    if (!n->has_method("_setup_from_entity")) {
+        std::string offending_node = fromGodotString(node->get_name());
+        logger.warn("The node " + offending_node + " has no method _setup_from_entity so cannot be auto-instantiated, but add_auto_instantiable_entity was called.");
+        return;
+    }
+    String resource_path = node->get_filename();
+    String default_metadata = resource_path.get_file();
+
+    worker::Entity entity;
+    current_entity_id++;
+    godotcore::GodotCoordinates2D position = toGlobalGodotPosition(n->get_global_position(), 0, 0);
+    entity.Add<godotcore::GodotPosition2D>({position, {}});
+    entity.Add<improbable::Position>(fromGodotPosition(position));
+    entity.Add<improbable::Persistence>({});
+    entity.Add<improbable::Metadata>({fromGodotString(default_metadata)});
+    entity.Add<godotcore::AutoInstantiable>({fromGodotString(resource_path)});
+    SchemaParser::applyComponentsToEntity(entity, n->call("_to_snapshot_entity"));
+    entity.Add<improbable::EntityAcl>({clientAndServerReqSet, make_component_acl(entity, "")});
+
+    snapshot_ostream->WriteEntity(current_entity_id, entity);
 }
 
 void SnapshotGenerator::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("take_snapshot", "absolute_path_to_snapshot"), &SnapshotGenerator::takeSnapshot);
+    ClassDB::bind_method(D_METHOD("new_snapshot", "absolute_path_to_snapshot"), &SnapshotGenerator::new_snapshot);
+    ClassDB::bind_method(D_METHOD("add_auto_instantiable_entity", "node"), &SnapshotGenerator::add_auto_instantiable_entity);
 }
 
 SnapshotGenerator::SnapshotGenerator() {
+}
+
+SnapshotGenerator::~SnapshotGenerator() {
+    if (snapshot_ostream != nullptr) {
+        logger.info("Finished writing snapshot.");
+        delete snapshot_ostream;
+    }
 }
--- modules/spatialos/snapshot_generator.h
+++ modules/spatialos/snapshot_generator.h
@@ -2,17 +2,24 @@
 #define SNAPSHOT_GENERATOR_H
 
 #include "editor_node.h"
+#include "improbable/worker.h"
+#include "worker_logger.h"
 
 class SnapshotGenerator : public Node {
     private:
         GDCLASS(SnapshotGenerator, Node);
+        static WorkerLogger logger;
+        worker::SnapshotOutputStream* snapshot_ostream;
+        std::uint64_t current_entity_id;
 
     protected:
         static void _bind_methods();
 
     public:
-        void takeSnapshot(String path);
+        void new_snapshot(String path);
+        void add_auto_instantiable_entity(Node* node);
         SnapshotGenerator();
+        ~SnapshotGenerator();
 };
 
 #endif
